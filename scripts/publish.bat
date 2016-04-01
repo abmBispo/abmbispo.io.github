4081e04762828e29a8d450b48a14df3e6f4d7037
Alan Borges <sr.alan.bispo@gmail.com>
2016-04-01 14:35:43 -0300

    Batch script fix

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,18 +1,9 @@
-@echo off
-
-npm >nul 2>&1 | echo "Npm/Node não está instalado!"
-
-npm list --global html-minifier >nul 2>&1 | echo "Requer html-minifier. Execute \"npm install html-minifier -g\""
-
-echo "\n~ATENÇÃO, usando estas configs:"
-type minifier.json
-echo
+﻿@echo off
 
 cd ..
-echo "...Linting:"
+
 html-minifier --output-dir . -c scripts/minifier.json --input-dir source
-echo
-echo "...Arquivos foram minificados"
+
 cd css
 type stylesheet.css > stylesheet.min.css
 type bootstrap-theme.css >> stylesheet.min.css
